0584b60e813de4e62d6ed812925d01d00c7d9e55
Audra Wathen <mrsbluerose@gmail.com>
2022-07-06 14:56:16 -0600

    integrates movie edit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Movie = require('./models/movie');
 
 mongoose.connect('mongodb://localhost:27017/movie-inventory');
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req,res) => {
     res.render('home');
@@ -51,7 +53,22 @@ app.get('/movies/:id', async (req, res) => {
     res.render('movies/show', { movie });
 })
 
+app.get('/movies/:id/edit', async (req, res) => {
+    const movie = await Movie.findById(req.params.id);
+    res.render('movies/edit', { movie });
+})
 
+app.put('/movies/:id', async (req, res) => {
+    const { id } = req.params;
+    const movie = await Movie.findByIdAndUpdate(id, { ...req.body.movie });
+    res.redirect(`/movies/${movie._id}`)
+})
+
+app.delete('/movies/:id', async (req, res) => {
+    const { id } = req.params;
+    await Movie.findByIdAndDelete(id);
+    res.redirect('/movies');
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
