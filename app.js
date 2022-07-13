4de7d5b3058e540684f3f0a479a16c1712a0bf02
Audra Wathen <mrsbluerose@gmail.com>
2022-07-13 16:25:36 -0600

    creates error template

--- app.js
+++ app.js
@@ -81,9 +81,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'Something went wrong'}  = err;
-    res.status(statusCode).send(message);
-    res.send("error error");
+    const { statusCode = 500 }= err;
+    if(!err.message) err.message = "An error occured";
+    res.status(statusCode).render('error', { err });
 })
 
 app.listen(3000, () => {
