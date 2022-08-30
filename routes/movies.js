b102541473565371d4a13cb9e0b908ae094efdbe
Audra Wathen <mrsbluerose@gmail.com>
2022-08-30 15:41:42 -0600

    cleans up code in routes

--- routes/movies.js
+++ routes/movies.js
@@ -7,18 +7,18 @@ const { isLoggedIn } = require('../middleware');
 const { validateMovie } = require('../middleware');
 const { isAuthor } = require('../middleware');
 
-router.get('/', catchAsync(movies.index));
+
+router.route('/')
+    .get(catchAsync(movies.index))
+    .post(isLoggedIn, validateMovie, catchAsync(movies.createMovie))
 
 router.get('/new', isLoggedIn, movies.renderNewForm);
 
-router.post('/', isLoggedIn, validateMovie, catchAsync(movies.createMovie));
-
-router.get('/:id', catchAsync(movies.showMovie));
+router.route('/:id')
+    .get(catchAsync(movies.showMovie))
+    .put(isLoggedIn, isAuthor, validateMovie, catchAsync(movies.updateMovie))
+    .delete(isLoggedIn, isAuthor, catchAsync(movies.deleteMovie))
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(movies.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateMovie, catchAsync(movies.updateMovie));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(movies.deleteMovie));
 
 module.exports = router;
