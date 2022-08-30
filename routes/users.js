4553470c21a294928e6050d398cc356d7640f528
Audra Wathen <mrsbluerose@gmail.com>
2022-08-30 15:43:58 -0600

    cleans up code in routes continued

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
+//const User = require('../models/user');
 const users = require('../controllers/users');
 
 router.route('/register')
