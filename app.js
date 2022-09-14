1985e2508d72cd9c9adc7f9a072241b44b0a6b34
Audra Wathen <mrsbluerose@gmail.com>
2022-09-14 10:38:17 -0600

    adds tmdb credentials and .env access

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+
+//use .env for development
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
